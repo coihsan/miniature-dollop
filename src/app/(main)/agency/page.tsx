0b3d8a3a499667a9148c6ed837a4deb82dbcc60b
M.Ihsan <110887888+coihsan@users.noreply.github.com>
2024-02-27 03:05:37 +0000

    first

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,11 +1,16 @@
 // import AgencyDetails from '@/components/forms/agency-details'
-// import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries'
+import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries'
 import { currentUser } from '@clerk/nextjs'
 // import { Plan } from '@prisma/client'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
-const Page = () =>{
+const Page = async () =>{
+    const authUser = await currentUser()
+    if(!authUser) return redirect('/sign-in')
+    const user = getAuthUserDetails()
+    const agencyId = await verifyAndAcceptInvitation()
+    console.log('login')
     return(
         <div>
         <h1>Agency</h1>
